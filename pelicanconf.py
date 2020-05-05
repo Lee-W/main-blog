626f8e4e519fc898216d2139e2a5493835a4e2ab
Wei Lee <weilee.rx@gmail.com>
2020-05-05 16:55:58 +0800

    theme: add license and update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,6 +25,13 @@ DISQUS_FILTER = False
 COMMENTS_INTRO = (
     "Do you like this article? What do your tink about it? Leave you comment below"
 )
+SITE_LICENSE = """
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
+</a>
+<br />
+This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+"""
 
 # Locale
 TIMEZONE = "Asia/Taipei"
