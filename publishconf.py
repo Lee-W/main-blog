5ea4450217e243a11ab9e895e0b5f1b9c726f827
Wei Lee <weilee.rx@gmail.com>
2024-08-21 23:24:00 +0800

    config: reorganize config and update config for theme attila

--- publishconf.py
+++ publishconf.py
@@ -8,21 +8,18 @@ import sys
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-
 sys.path.append(os.curdir)
 from pelicanconf import *  # noqa: F401, E402, F403
 
-SITEURL = "https://blog.wei-lee.me"
+SITEURL = f"https://{HOST}"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
-DELETE_OUTPUT_DIRECTORY = False
-
 EXTRA_PATH_METADATA = {
     "extra/CNAME": {"path": "CNAME"},
 }
-PAGEFIND_ENABLED = True
-# Following items are often useful when publishing
-MODERN_GOOGLE_ANALYTICS = os.environ.get("GOOGLE_ANALYTICS")
+DELETE_OUTPUT_DIRECTORY = True
+
+GOOGLE_ANALYTICS = os.environ.get("GOOGLE_ANALYTICS")
